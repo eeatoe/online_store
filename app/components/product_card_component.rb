d7a17e915f9cca415a25406dacb954e7a70477ec
Sergey Smirnov <eeatoe@gmail.com>
2025-02-05 05:54:36 +0100

    Product path development

--- app/components/product_card_component.rb
+++ app/components/product_card_component.rb
@@ -4,12 +4,4 @@ class ProductCardComponent < ViewComponent::Base
   def initialize(product:)
     @product = product
   end
-
-  def image_url
-    if @product.images.any
-      @product.images.first.attachment.url
-    else
-      "placeholder.jpg"
-    end
-  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,14 @@
 class ProductsController < ApplicationController
+  before_action :set_active_storage_url_options
+
   def index
     @products = Spree::Product.all
+  end
+
+  private
+
+  def set_active_storage_url_options
+    ActiveStorage::Current.url_options = { host: request.base_url }
   end
 end
 
